cda8fd707f58942ef39eb756f909389ef2a8aa4f
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-09-03 11:33:00 +0000

    index range

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """
-    0-simple_helper_function.py: a function that takes two integer arguments page and page_size
+    0-simple_helper_function.py: a function that takes two integer arguments
+    page and page_size
 """
+
 
 def index_range(page: int, page_size: int) -> tuple[int, int]:
     """
-        page: the page 
+        page: the page
         page_size: and page_size
     """
     return ((page * page_size) - page_size, page_size)
