07171dba9a959300071e23c87c3233c35e24c386
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-09-03 11:17:10 +0000

    Update 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python3
+"""
+    0-simple_helper_function.py: a function that takes two integer arguments page and page_size
+"""
 
+def index_range(page, page_size):
+    """
+        page: the page 
+        page_size: and page_size
+    """
+    return ((page * page_size) - page_size, page_size)
