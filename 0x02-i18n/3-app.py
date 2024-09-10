79a4e5383f0a4fa2e6b4525c4a439e306b888f91
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-09-10 13:51:55 +0000

    Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,37 @@
+"""
+    0-app.py: a basic flask application
+
+"""
+from flask_babel import Babel
+from flask import Flask
+from flask import request
+from flask import render_template
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.Config["LANGUAGES"])
+
+
+@app.route("/")
+def home():
+    """
+        render an index template
+    """
+    return render_template("1-index.html")
+
+
+if __name__ == '__main__':
+    app.run()
